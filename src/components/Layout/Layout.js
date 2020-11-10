8e6547c0090cc9b7142775a87039b07ebe03c100
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-11-10 13:34:13 +0100

    fix(layout): added scroll to top on location change

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import styles from './Layout.module.css';
 import logo from '../../images/ic-logo-white.png';
 import poweredBy from '../../images/powered-by-ic.png';
 
-const Layout = ({ children }) => (
-  <div className={styles.container}>
-    <header className={styles.header}>
-      <img src={logo} alt="InfoCaption Logo" className={styles.logo} />
-    </header>
-    
-    <main className={styles.main}>
-      {children}
-    </main>
+const Layout = ({ children }) => {
+  // Scroll to top on location-change
+  const { pathname, search } = useLocation();
+  useEffect(() => window.scrollTo(0, 0), [pathname, search]);
 
-    <footer className={styles.footer}>
-      <img src={poweredBy} alt="Powered by InfoCaption" />
-      <p>Built by Andreas Åkerlöf</p>
-    </footer>
-  </div>
-);
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <img src={logo} alt="InfoCaption Logo" className={styles.logo} />
+      </header>
+      
+      <main className={styles.main}>
+        {children}
+      </main>
+
+      <footer className={styles.footer}>
+        <img src={poweredBy} alt="Powered by InfoCaption" />
+        <p>Built by Andreas Åkerlöf</p>
+      </footer>
+    </div>
+  );
+}
 
 export default Layout;
 
