255e08c0af185b7be66cdcedef20685322e02c47
Andreas Åkerlöf <a.akerlof@gmail.com>
2020-11-11 15:24:23 +0100

    feat(layout): added link to logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Layout.module.css';
 import logo from '../../images/ic-logo-white.png';
 import poweredBy from '../../images/powered-by-ic.png';
@@ -14,7 +14,9 @@ const Layout = ({ children }) => {
     <div className={styles.container}>
       <header className={styles.header}>
         <div className={styles.headerBlock}>
-          <img src={logo} alt="InfoCaption Logo" className={styles.logo} />
+          <Link to="/">
+            <img src={logo} alt="InfoCaption Logo" className={styles.logo} />
+          </Link>
         </div>
       </header>
       
